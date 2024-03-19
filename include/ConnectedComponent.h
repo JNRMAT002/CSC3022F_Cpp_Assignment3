b450bec09cb126b1f5381f3296f82f1f012038a3
JNRMAT002 <matthewjanuary@gmail.com>
2024-03-19 15:50:33 +0200

    Created definition and skeleton implementation for ConnectedComponent.

---
+++ include/ConnectedComponent.h
@@ -0,0 +1,37 @@
+// Definition file for ConnectedComponent (Assignment 3 for CSC3022F, 2024)
+// By Matthew January - JNRMAT002
+
+#ifndef ConnectedComponent_H
+#define ConnectedComponent_H
+
+#include <iostream>
+#include <vector>
+
+namespace JNRMAT002 {
+    class ConnectedComponent{
+        // Naming all member variables with "m_" to distinguish from function parameters.
+        private:
+            int m_numPixels; // Number of pixels in a component
+            int m_compID; // Unique integer ID for a component
+            std::vector<std::pair<unsigned char, int>> compPixels; // Storing a vector of pairs - pair is <pixel, index buffer>
+        public:
+            // Constructor
+            ConnectedComponent(int compID);
+
+            // Destructor
+            ~ConnectedComponent();
+
+            // GETTERS
+            int getNumPixels();
+            int getCompID();
+            std::vector<std::pair<unsigned char, int>> getCompPixels();
+
+            // SETTERS
+            void setNumPixels();
+            void setCompID(int compID);
+            void addPixel(unsigned char pixel, int index);
+
+    };
+}
+
+#endif
---
+++ src/ConnectedComponent.cpp
@@ -0,0 +1,39 @@
+// Implementation file for ConnectedComponent (Assignment 3 for CSC3022F, 2024)
+// By Matthew January - JNRMAT002
+
+#include "../include/ConnectedComponent.h"
+
+using namespace JNRMAT002;
+
+ConnectedComponent::ConnectedComponent(int compID) {
+    std::cout << "Creating component number " << compID << "." << std::endl;
+    m_numPixels = 0;
+    setCompID(compID);
+}
+
+ConnectedComponent::~ConnectedComponent() {
+
+}
+
+// GETTERS
+int ConnectedComponent::getNumPixels() {
+    return m_numPixels;
+}
+int ConnectedComponent::getCompID() {
+    return m_compID;
+}
+std::vector<std::pair<unsigned char, int>> ConnectedComponent::getCompPixels() {
+    return compPixels;
+}
+
+// SETTERS
+void ConnectedComponent::setNumPixels() {
+    m_numPixels++;
+}
+void ConnectedComponent::setCompID(int compID) {
+    m_compID = compID;
+}
+void ConnectedComponent::addPixel(unsigned char pixel, int index) {
+    // Add to vector here
+    setNumPixels();
+}
